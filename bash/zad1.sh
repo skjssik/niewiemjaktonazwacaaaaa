3cf9eedbf0b8226e16b6a72fe9fb187bd4cc2f05
Karol Szwarc <klops100000@gmail.com>
2022-09-21 22:28:39 +0200

    ostatcznie 1 zadanie

--- bash/zad1.sh
+++ bash/zad1.sh
@@ -1,2 +1,33 @@
-tu bedzie zadanie 1 
-a moze nie będzie hihi
+    #!/bin/bash
+
+    read n
+    
+    let n=n+1
+
+    for (( c=0; c<$n; c++ ))
+    do
+         S[c]=1
+    done
+    g=$(echo "sqrt ( $n)" | bc -l) ; 
+     for (( i=2; i<$n; i++ ))
+    do
+     
+    if [[ " ${S[i]} " -eq 1 ]]; then
+       let w=i*i
+       while [ $w -le $n ]; do
+     
+     S[w]=0 
+     let w=w+i
+     
+    done
+     
+    fi
+     
+     
+    done
+     for (( i=2; i<=$n; i++ ))
+    do
+       if [[ " ${S[i]} " -eq 1 ]]; then
+       echo " $i "
+       fi
+    done
